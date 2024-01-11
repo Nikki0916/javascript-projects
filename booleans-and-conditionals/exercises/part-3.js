36f64eb8914dfceb734a63782fcf83f21e46ac14
Nicole Wade <nwade0916@gmail.com.com>
2024-01-11 00:31:17 -0600

    data and v exercises conditionals

--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -6,16 +6,41 @@ let engineTemperature = 1200;
 
 a) If fuelLevel is above 20000 AND engineTemperature is at or below 2500, print "Full tank. Engines good."
 
+if (fuelLevel >20000 && engineTempature <=2500 ) {
+   console.log("Full tank. Engines good.");
+} ;
+
 b) If fuelLevel is above 10000 AND engineTemperature is at or below 2500, print "Fuel level above 50%.  Engines good."
 
+if (fuelLevel >10000 && engineTempature <=2500 ) {
+   console.log("Fuel level above 50%.  Engines good.");
+} ;
 c) If fuelLevel is above 5000 AND engineTemperature is at or below 2500, print "Fuel level above 25%. Engines good."
-
+if (fuelLevel >5000 && engineTempature <=2500 ) {
+   console.log("Fuel level above 25%. Engines good.");
+} ;
 d) If fuelLevel is at or below 5000 OR engineTemperature is above 2500, print "Check fuel level. Engines running hot."
-
+if (fuelLevel =<5000 || engineTempature >2500 ) {
+   console.log("Check fuel level. Engines running hot.");
+} ;
 e) If fuelLevel is below 1000 OR engineTemperature is above 3500 OR engineIndicatorLight is red blinking print "ENGINE FAILURE IMMINENT!" 
-
+if (fuelLevel < 1000) {
+  console.log("ENGINE FAILURE IMMINENT!");
+} else if (engineTemperature > 3500) {
+  console.log("ENGINE FAILURE IMMINENT!");
+} else engineIndicatorLight == "red blinking" {
+  console.log("ENGINE FAILURE IMMINENT!");
+}
 f) Otherwise, print "Fuel and engine status pending..." */
+if (fuelLevel < 1000) {
+    console.log("ENGINE FAILURE IMMINENT!");
+  } else if (engineTemperature > 3500) {
+    console.log("ENGINE FAILURE IMMINENT!");
+  } else if (engineIndicatorLight == "red blinking") {
+    console.log("ENGINE FAILURE IMMINENT!"); 
+  } else("Fuel and engine status pending...")
 
+    
 // Code 5a - 5f here:
 
 // 6) a) Create the variable commandOverride, and set it to be true or false. If commandOverride is false, then the shuttle should only launch if the fuel and engine check are OK. If commandOverride is true, then the shuttle will launch regardless of the fuel and engine status.
