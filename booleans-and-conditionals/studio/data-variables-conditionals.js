e1917f8b3f8e28221d0d8dd8e0658bd3a11002f9
Nicole Wade <nwade0916@gmail.com.com>
2024-01-11 22:36:02 -0600

    studio work for conditionals

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,43 +1,47 @@
 // Initialize Variables below
-let date = 	Monday 2019-03-18
-let time = 10:05:34 AM
-let astronautCount = 7
-let astronautStatus = ready
-let averageAstronautMassKg = 80.7
-let crewMassKg = astronautCount * averageAstronautMassKg
-let fuelMassKg = 760,000
-let shuttleMassKg = 74842.31
-let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg
-let maximumMassLimit = 850000
-let fuelTempCelsius = -225
-let minimumFuelTemp = -300
-let maximumFuelTemp = -150
-let fuelLevel = 100%
-let weatherStatus = clear
-let preparedForLiftOff = true
+let date = 	"Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = "100%";
+let weatherStatus = "clear";
+let preparedForLiftOff = "true";
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-if (astronautCount > 7) {
-    console.log("Crew Ready");
-  }
+if (astronautCount >= 7) {
+  console.log ("astronauts are ready")
+
+}
 // add logic below to verify all astronauts are ready
-if ( astronautStatus = ready) {
-    console.log("Crew Ready");
-  }
+if ( astronautStatus = "ready") {
+console.log ("astronauts are ready")
+} else if (astronautCount <= 7) 
+console.log ("Too many astronauts") 
+
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
-if ( totalMassKg <850000) {
-    console.log("Crew Ready");
-  }
+if (totalMassKg <= 850000) {
+  console.log ("weight is within limit")
+ }
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-if ( totalMassKg < -150 & < -300) {
-    console.log("Crew Ready");
+if ( fuelTempCelsius >= minimumFuelTemp && fuelTempCelsius <= maximumFuelTemp ) {
+    console.log("Temp Okay");
   }
 // add logic below to verify the fuel level is at 100%
-if ( fuelLevel = 100%) {
-    console.log("Crew Ready");
+if ( fuelLevel === "100%") {
+    console.log("Fuel is full");
   }
 // add logic below to verify the weather status is clear
-if ( weatherStatus = 100%) {
-    console.log("Crew Ready");
+if ( weatherStatus === "clear") {
+    console.log("Weather is clear");
   }
 // Verify shuttle launch can proceed based on above conditions
+if 
