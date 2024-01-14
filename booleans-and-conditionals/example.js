063b58e76841a6d2af7abc0ef4f29d2130190a66
Nicole Wade <nwade0916@gmail.com.com>
2024-01-14 16:24:38 -0600

    shuttle launch

---
+++ booleans-and-conditionals/example.js
@@ -0,0 +1,35 @@
+let num =  6
+
+if (num % 2 === 0) {
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
+console.log(4 < 3 || 2 < 3)
+let billHasBeenPaid = false;
+
+if (!billHasBeenPaid) {
+   console.log("Your bill is due soon!");
+}
+if (num % 2 === 0 && num > 3) {
+    console.log(num, "is even");
+    console.log(num, "is greater than 3");
+ }
+ let x = 10;
+let y = 20;
+
+if (x > y) {
+   console.log("x is greater than y");
+} else if (x < y) {
+   console.log("x is less than y");
+} else {
+   console.log("x and y are equal");
+}
+let a = 7;
+if (a % 2 === 1) {
+   console.log("Launch");
+} else if (a > 5) {
+   console.log("Code");
+} else {
+   console.log("LaunchCode");
+}
