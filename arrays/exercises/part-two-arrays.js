a53d8ef937a8c59fa501529925be6508d4eaaca7
Nicole Wade <nwade0916@gmail.com.com>
2024-01-17 23:14:49 -0600

    class on 1 18 24

--- arrays/exercises/part-two-arrays.js
+++ arrays/exercises/part-two-arrays.js
@@ -1,11 +1,20 @@
 let cargoHold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket'];
 
 //1) Use bracket notation to replace ‘slinky’ with ‘space tether’. Print the array to confirm the change.
-
+cargoHold[5] = 'space tether';
+console.log(cargoHold);
 //2) Remove the last item from the array with pop. Print the element removed and the updated array.
-
+let removedElement = cargoHold.pop();
+console.log('Removed Element:', removedElement);
+console.log('Updated Array:', cargoHold);
 //3) Remove the first item from the array with shift. Print the element removed and the updated array.
-
+let removedElementS = cargoHold.shift();
+console.log('Removed Element:', removedElementS);
+console.log('Updated Array:', cargoHold);
 //4) Unlike pop and shift, push and unshift require arguments inside the (). Add the items 1138 and ‘20 meters’ to the the array - the number at the start and the string at the end. Print the updated array to confirm the changes.
-
+cargoHold.unshift(1138); 
+cargoHold.push('20 meters'); 
+console.log(cargoHold);
 //5) Use a template literal to print the final array and its length.
+// console.log(`The array ${cargoHold} has a length of ${cargoHold.length}.`);
+console.log(`The array ${cargoHold} has a length of ${cargoHold.length}.`);
---
+++ stringing-characters-together/code-snippets/phrase = 'Code for fun'.js
@@ -0,0 +1,173 @@
+// phrase = 'Code for fun'
+// console.log(phrase[2])
+
+// myStr = 'Index'
+// console.log(myStr[2] === 'n');
+// console.log(myStr[4] === 'x');
+// console.log(myStr[6] === ' ');
+// console.log(myStr[0] === 'I');
+
+// let phrase = "JavaScript rocks!";
+// console.log(phrase[phrase.length - 8]);
+
+// cityName = "Vienna";
+// stateName = "Virginia";
+// location = cityName + ", " +  stateName;
+
+// console.log(location.length);
+
+// word = 'JavaScript';
+// console.log(word.slice(4).toUpperCase());
+
+// let language = "JavaScript";
+// language.replace('J', 'Q');
+// language.slice(0,5);
+// console.log(language);
+
+// language = 'JavaScript';
+// console.log(language.slice(1,6));
+
+// let org = "  The LaunchCode Foundation ";
+// let trimmed = org.trim();
+
+// console.log(trimmed);
+
+
+// console.log('Launch\nCode');
+// console.log('Launch/nCode');
+// console.log('Launch', 'Code');
+// console.log('Launch\tCode');
+// console.log('Launch/tCode');
+
+// let nonprofit = "LaunchCode";
+
+// console.log(nonprofit.charCodeAt(0));
+// console.log(nonprofit.charCodeAt(1));
+// console.log(nonprofit.charCodeAt(2));
+// console.log(nonprofit.charCodeAt(3));
+// console.log(nonprofit.charCodeAt(4));
+// console.log(nonprofit.charCodeAt(5));
+// console.log(nonprofit.charCodeAt(6));
+// console.log(nonprofit.charCodeAt(7));
+// console.log(nonprofit.charCodeAt(8));
+// console.log(nonprofit.charCodeAt(9));
+
+// let codes = [76, 97, 117, 110, 99, 104, 67, 111, 100, 101];
+
+// let characters = String.fromCharCode(codes[0]) + String.fromCharCode(codes[1])
+//                 + String.fromCharCode(codes[2]) + String.fromCharCode(codes[3])
+//                 + String.fromCharCode(codes[4]) + String.fromCharCode(codes[5])
+//                 + String.fromCharCode(codes[6]) + String.fromCharCode(codes[7])
+//                 + String.fromCharCode(codes[8]) + String.fromCharCode(codes[9]);
+
+// console.log(characters);
+
+// console.log("A message\nbroken across lines,\n\tand indented");
+
+// console.log("The interrobang character, \u203d, combines ? and !");
+
+// console.log("\"The dog's favorite toy is a stuffed hedgehog,\" said Chris");
+
+// let name = "Jack";
+// let currentAge = 9;
+
+// console.log("Next year, " + name + " will be " + (currentAge + 1) + ".");
+
+// let name = "Jack";
+// let currentAge = 9;
+
+// console.log(`Next year, ${name} will be ${currentAge + 1}.`);
+
+// let poem = `The mind chases happiness.
+// The heart creates happiness.
+// The soul is happiness
+// And it spreads happiness
+// All-where.
+
+// – Sri Chinmoy`;
+
+// console.log(poem);
+
+// let pluralNoun = "dogs";
+// let name = "Blake";
+// let verb = "Jump" ;
+// let adjective = "Salty";
+// let color = "blue";
+// console.log(`JavaScript provides a ${color} collection of tools - including ${adjective} syntax and ${pluralNoun} - that allows ${name} to ${verb} ith strings.`);
+
+// console.log(`JavaScript provides a ${color} collection of tools - including ${adjective} syntax and ${pluralNoun} - that allows ${name} to ${verb} with strings.`);
+
+// let emptyArray = [];
+// console.log(emptyArray.length);
+
+// let programmingLanguages = ["JavaScript", "Python", "Java", "C#"];
+// console.log(programmingLanguages.length);
+
+// let grabBag = ["A string value", true, 99, 105.5];
+// console.log(grabBag.length);
+
+// let classes = ["science", "computer", "art"];
+// console.log(classes.length)
+
+// let teachers = ["Jones", "Willoughby", "Rhodes"];
+// console.log(teachers.length)
+
+// let programmingLanguages = [
+//     "JavaScript", // index 0
+//     "Python",     // index 1
+//     "Java",       // index 2
+//     "C#"          // index 3
+// ];
+// console.log(programmingLanguages[0]);
+// console.log(programmingLanguages[3]);
+
+// // What will happen when index 4 is requested?
+// console.log(programmingLanguages[4]);
+
+// let programmingLanguages = ["JavaScript", "Python", "Java", "C#"];
+// console.log(programmingLanguages[-1]);
+// console.log(programmingLanguages[100]);
+
+// let javaScriptFrameworks = ["React", "Angular", "Ember"];
+// console.log(javaScriptFrameworks);
+
+// // Set the value of index 2 to be "Vue"
+// javaScriptFrameworks[2] = "Vue";
+
+// // Notice the value at index 2 is now "Vue"
+// console.log(javaScriptFrameworks);
+
+// let charles = ['coder', 'Tech', 47, 23, 350];
+// charles.sort();
+// console.log(charles);
+
+// str = 'LaunchCode students rock!'
+// str.split("");
+// console.log(str)
+
+//
+// let shuttleCrews = [
+//     ['Robert Gibson', 'Mark Lee', 'Mae Jemison'],
+//     ['Kent Rominger', 'Ellen Ochoa', 'Bernard Harris'],
+//     ['Eilen Collins', 'Winston Scott',  'Catherin Coleman']
+// ];
+
+// console.log(shuttleCrews[0][2]);
+// console.log(shuttleCrews[1][1]);
+// console.log(shuttleCrews[2][1]);
+
+let shuttleCrews = [
+    ['Robert Gibson', 'Mark Lee', 'Mae Jemison'],
+    ['Kent Rominger', 'Ellen Ochoa', 'Bernard Harris'],
+    ['Eilen Collins', 'Winston Scott',  'Catherin Coleman']
+];
+
+let newCrew = ['Mark Polansky', 'Robert Curbeam', 'Joan Higginbotham'];
+
+// Add a new crew array to the end of shuttleCrews
+shuttleCrews.push(newCrew);
+console.log(shuttleCrews[3][2]);
+
+// Reverse the order of the crew at index 1
+shuttleCrews[1].reverse();
+console.log(shuttleCrews[1]);
