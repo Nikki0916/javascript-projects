1589055be7abe59f80359e1249124d888e250622
Nicole Wade <nwade0916@gmail.com.com>
2024-01-22 18:11:35 -0600

    updates

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,27 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+let answer = str.slice(3).concat(str.slice(0,3))
+console.log(answer)
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`Response ${answer}`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-
+let quantity = input.question('please input letters to move:')
+answer =  str.slice(quantity).concat(str.slice(0,quantity))
+console.log(answer)
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (quantity < 0) {
+    console.log(
+        `Part 3 :due to input being less then the string length you get the default answer: ${answer}`
+        );
+    }
+        else if (quantity > str.length) {
+            console.log(
+            `part 3 error due to the input being lrger then the length`
+            );
+
+        }
+
+
+console.log(answer);
--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,8 +4,27 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
+  //  a 
+   for (let i = 0; i <= 20; i++) {
+      console.log(i);
+   }
 
+  //  b
+  for (let i = 3; i <= 29; i += 2) {
+    console.log(i);
+ }
+ 
+  //  C
+  for (let i = 12; i >= -14; i-=2) {
+    console.log(i);
+ }
 
+//  D
+for (let i = 50; i >= 20; i--) {
+  if (i % 3 === 0) {
+     console.log(i);
+  }
+}
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
@@ -15,10 +34,40 @@ Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
+  let str = "LaunchCode";
+  let arr = [1, 5, 'LC101', 'blue', 42];
+
+  // A
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
 
 
+// b
+console.log("\nCharacters of the string in reverse order:");
+for (let i = str.length - 1; i >= 0; i--) {
+   console.log(str[i]);
+}
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  let numbers = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evenNumbers = [];
+  let oddNumbers = [];
+ 
+  // A
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+       evenNumbers.push(numbers[i]);
+    } else {
+       oddNumbers.push(numbers[i]);
+    }
+ }
+
+  // B
+
+  console.log("Even numbers array:", evenNumbers);
+console.log("Odd numbers array:", oddNumbers);
