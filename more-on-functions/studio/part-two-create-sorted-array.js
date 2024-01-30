4f1d3ea3e26b3d68da898537a1f347705e8673cf
Nicole Wade <nwade0916@gmail.com.com>
2024-01-29 19:46:41 -0600

    test

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -18,6 +18,7 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
+
 console.log(findMin(nums1))
 console.log(findMin(nums2))
 console.log(findMin(nums3))
