b97e5dde0ba7526f849104b7f5a5e709e881de72
Nicole Wade <nwade0916@gmail.com.com>
2024-02-26 19:38:33 -0600

    DOM excerises completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,26 @@
-function init () {
+function init() {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    button.addEventListener('click', function(event) {
+        paragraph.textContent = 'Houston! We have liftoff!';
+    });   
+
+    missionAbort.addEventListener('mouseover', function(event) {
+        missionAbort.style.backgroundColor = 'red';
+    });   
+
+    missionAbort.addEventListener('mouseout', function(event) {
+        event.target.style.backgroundColor = '';
+    });
+
+    missionAbort.addEventListener('click', function(event) {
+        let confirmation = confirm('Are you sure you want to abort the mission?');
+        if (confirmation) {
+            paragraph.textContent = 'Mission Aborted! Space shuttle returning home.';
+        }
+    });
 }
 
 window.addEventListener("load", init);
